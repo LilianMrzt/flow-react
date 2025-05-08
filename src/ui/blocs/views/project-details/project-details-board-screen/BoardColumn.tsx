770cc3fe3f13b8cf2dc5ff098ba049e29f3b1698
Lilian Marzet <lilian.marzet123@gmail.com>
2025-05-08 22:51:26 +0200

    [FEAT] Header des colonnes du Board

--- src/ui/blocs/views/project-details/project-details-board-screen/BoardColumn.tsx
+++ src/ui/blocs/views/project-details/project-details-board-screen/BoardColumn.tsx
@@ -1,11 +1,27 @@
 import React, { ReactNode } from 'react'
 import './board-column.css'
+import Text from '@components/text/Text'
+
 const BoardColumn = (): ReactNode => {
     return (
         <div
             className={'board-column'}
         >
+            <div
+                className={'board-column-header'}
+            >
+                <Text>
+                    A faire
+                </Text>
+                <Text>
+                    0
+                </Text>
+            </div>
+            <div
+                className={'board-column-content'}
+            >
 
+            </div>
         </div>
     )
 }
